0fd7bc8ecda6a7c0542ceb062392836171045894
pravin <parvindraj004@gmail.com>
2022-06-10 19:50:28 +0530

    git commit-m

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         //primarySwatch: Colors.blue,
       ),
-      home:  HomeScreen(),
+      home:const  HomeScreen(),
     );
   }
 }
